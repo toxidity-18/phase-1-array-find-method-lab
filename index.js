5e6675af76ecc1eae135e04e339bce36b9c8a1be
toxidity-18 <desavigatunganga@gmail.com>
2025-05-13 10:25:32 +0300

    ...

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 
-function superbowlWin(record) {
-    const winningYear = record.find(year => year.result === "W");
-    return winningYear ? winningYear.year : undefined;
-  }
+// function superbowlWin(record) {
+//     const winningYear = record.find(year => year.result === "W");
+//     return winningYear ? winningYear.year : undefined;
+//   }
   
+
+  // using find to isolate specific results 
+  // array of js objects ...
+  const record = [
+    {year:'2015',result:'W'},
+    {year:'2013',result:'L'},
+    {year:'2014',result:'N/A'}
+    // . . .
+  ]
+  function superbowlWin(record){
+    const win= record.find(year => year.result === 'W');
+    return win ? win.year : undefined;
+  }
